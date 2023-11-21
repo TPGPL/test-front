0cd4aba82c6814af489b64eb8ddd450ad7c174f1
TPGPL <TPGPL@users.noreply.github.com>
2023-11-21 19:36:27 +0100

    Add some base services

---
+++ Services/ClientService.cs
@@ -0,0 +1,122 @@
+﻿using OrderManagement.Models;
+
+namespace OrderManagement.Services;
+
+public class ClientService : IClientService
+{
+    private HttpClient _httpClient;
+
+    public ClientService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<ServiceResponse<List<Client>>> GetClientsAsync()
+    {
+        try
+        {
+            var response = await _httpClient.GetAsync("clients");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return new ServiceResponse<List<Client>>
+                {
+                    Success = false,
+                    Message = "HTTP Request failed."
+                };
+            }
+
+            var result = await response.Content.ReadFromJsonAsync<ServiceResponse<List<Client>>>()
+                ?? new ServiceResponse<List<Client>>() { Success = false, Message = "Failed to read data." };
+
+            result.Success = result.Success && result.Data is not null;
+
+            return result;
+        }
+        catch (Exception ex)
+        {
+            return new ServiceResponse<List<Client>>()
+            {
+                Success = false,
+                Message = ex.Message ?? "Unknown error occured."
+            };
+        }
+    }
+
+    public async Task<ServiceResponse<Client>> GetClientAsync(int id)
+    {
+        try
+        {
+            var response = await _httpClient.GetAsync($"clients/{id}");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return new ServiceResponse<Client>
+                {
+                    Success = false,
+                    Message = "HTTP Request failed."
+                };
+            }
+
+            var result = await response.Content.ReadFromJsonAsync<ServiceResponse<Client>>()
+                ?? new ServiceResponse<Client>() { Success = false, Message = "Failed to read data." };
+
+            result.Success = result.Success && result.Data is not null;
+
+            return result;
+        }
+        catch (Exception ex)
+        {
+            return new ServiceResponse<Client>
+            {
+                Success = false,
+                Message = ex.Message ?? "Unknown error occurred."
+            };
+        }
+    }
+
+    public async Task<ServiceResponse<Client>> UpdateClientAsync(int id, Client author)
+    {
+        try
+        {
+            var response = await _httpClient.PutAsJsonAsync($"clients/{id}", author);
+            var result = await response.Content.ReadFromJsonAsync<ServiceResponse<Client>>()
+                ?? new ServiceResponse<Client>() { Success = false, Message = "Failed to read data." };
+
+            return result;
+        }
+        catch (Exception ex)
+        {
+            return new ServiceResponse<Client>
+            {
+                Success = false,
+                Message = ex.Message ?? "Unknown error occurred."
+            };
+        }
+    }
+
+    public async Task<ServiceResponse<Client>> CreateClientAsync(Client author)
+    {
+        try
+        {
+            var response = await _httpClient.PostAsJsonAsync($"clients", author);
+            var result = await response.Content.ReadFromJsonAsync<ServiceResponse<Client>>()
+                ?? new ServiceResponse<Client>() { Success = false, Message = "Failed to read data." };
+
+            return result;
+        }
+        catch (Exception ex)
+        {
+            return new ServiceResponse<Client>
+            {
+                Success = false,
+                Message = ex.Message ?? "Unknown error occurred."
+            };
+        }
+    }
+
+    public async Task DeleteClientAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Services/OrderService.cs
@@ -0,0 +1,53 @@
+﻿using OrderManagement.Models;
+
+namespace OrderManagement.Services;
+
+public class OrderService : IOrderService
+{
+    private readonly HttpClient _httpClient;
+
+    public OrderService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public Task CancelOrderAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task CompleteOrderAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ServiceResponse<Order>> CreateOrderAsync(Order order)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteOrderAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ServiceResponse<Order>> GetOrderAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ServiceResponse<List<Order>>> GetOrdersAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SubmitOrderAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ServiceResponse<Order>> UpdateOrderAsync(int id, Order order)
+    {
+        throw new NotImplementedException();
+    }
+}
