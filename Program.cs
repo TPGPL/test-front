8bc0a5486b3f70a4047f8197efdc171f61902431
TPGPL <TPGPL@users.noreply.github.com>
2023-11-21 19:40:29 +0100

    Add DI services

--- Program.cs
+++ Program.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using OrderManagementApp;
+using OrderManagementApp.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton(sp => new HttpClient() { BaseAddress = new Uri("http://localhost:8080/") });
+builder.Services.AddSingleton<IOrderService, OrderService>();
+builder.Services.AddSingleton<IClientService, ClientService>();
+
 
 await builder.Build().RunAsync();
